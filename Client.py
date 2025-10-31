35c48890b5fccba4cece229f2d5dc10ea85be643
deepakoffl007 <deepakelumalai.e@gmail.com>
2025-10-31 08:58:27 +0530

    app practice

---
+++ Client.py
@@ -0,0 +1,7 @@
+import socket
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect(('localhost',12345))
+client_socket.send("Hello server".encode())
+data = client_socket.recv(1024).decode()
+print(f"Received from server : {data}")
+client_socket.close()
---
+++ Server.py
@@ -0,0 +1,11 @@
+import socket 
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.bind(('localhost',12345))
+server_socket.listen(1)
+print("Server is Listening port 12345...")
+conn, addr = server_socket.accept()
+print(f"connected with {addr}")
+data = conn.recv(1024).decode()
+print(f"received from the client{data}")
+conn.send("Hello client from the server your message Received...".encode())
+conn.close()
